3e3e75f3b73f7f0133dd1299ad8de66e23678a64
Steven Francius <stevenfrancius@gmail.com>
2025-04-22 09:40:43 -0400

    dded funaction to render to do list

--- script.js
+++ script.js
@@ -7,4 +7,15 @@ const todoList = [{
   }];
   
   renderTodoList();
+  function renderTodoList() {
+    let todoListHTML = '';
   
+    todoList.forEach((todoObject, index) => {
+      const { name, dueDate } = todoObject;
+      const html = `
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button class="delete-todo-button js-delete-todo-button">Delete</button> 
+      `;
+      todoListHTML += html;
+    });
